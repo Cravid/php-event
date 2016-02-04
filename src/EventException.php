ee8e06b12618503ef96c9e5f2eb0c4876421d04d
Cravid <github@schunke.me>
2016-02-04 10:44:03 +0100

    Update EventException.php

--- src/EventException.php
+++ src/EventException.php
@@ -2,6 +2,23 @@
 
 namespace Cravid\Event;
 
-class EventException extends \Exception
+class EventException extends \RuntimeException
 {
+    /**
+     * Event instance.
+     *
+     * @var EventInterface
+     */
+    protected $event;
+
+
+    /**
+     * @param EventInterface $event The event instance.
+     */
+    public function __construct(EventInterface $event)
+    {
+        $this->event = $event;
+
+        parent::__construct(sprintf('Exception in event "%s".', $event->getName()));
+    }
 }
